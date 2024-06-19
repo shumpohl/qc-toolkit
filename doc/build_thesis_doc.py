f1217fcc45dd7928ca13e0517a99ec967dfbe37e
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2024-06-19 17:20:26 +0200

    Incredibly bad script WIP

--- doc/build_thesis_doc.py
+++ doc/build_thesis_doc.py
@@ -3,12 +3,46 @@ import json
 import sys
 import pathlib
 import shutil
+import argparse
 
 doc_root = pathlib.Path(__file__).parent.absolute()
 
 original_source = doc_root / 'source'
 thesis_source = doc_root / 'thesis_source'
-build = doc_root / 'build'
+thesis_build = doc_root / 'thesis_build'
+
+REDEFINE_RM = r'''
+\renewcommand{\rmdefault}{\rmfamily}
+\renewcommand{\sfdefault}{\sffamily}
+\renewcommand{\ttdefault}{\ttfamily}
+'''
+
+KOMA_SECTION_FORMAT = r'''
+% Use KOMA-Script's section formatting
+\RedeclareSectionCommand[
+beforeskip=-3.5ex plus -1ex minus -.2ex,
+afterskip=2.3ex plus .2ex
+]{section}
+\RedeclareSectionCommand[
+beforeskip=-3.25ex plus -1ex minus -.2ex,
+afterskip=1.5ex plus .2ex
+]{subsection}
+\RedeclareSectionCommand[
+beforeskip=-3.25ex plus -1ex minus -.2ex,
+afterskip=1.5ex plus .2ex
+]{subsubsection}
+\RedeclareSectionCommand[
+beforeskip=-3.25ex plus -1ex minus -.2ex,
+afterskip=1.5ex plus .2ex
+]{paragraph}
+\RedeclareSectionCommand[
+beforeskip=-3.25ex plus -1ex minus -.2ex,
+afterskip=1.5ex plus .2ex
+]{subparagraph}'''
+
+TEXT_COMP = r"""
+\usepackage{textcomp}
+"""
 
 settings = dict(
     latex_engine = 'lualatex',
@@ -22,49 +56,76 @@ settings = dict(
 
         # Additional stuff for the LaTeX preamble.
             'extraclassoptions': 'openany',  # Optional: Remove blank pages between chapters
-            'preamble': r'''
-            % Disable the default title and author
-            \renewcommand{\maketitle}{}
-            \renewcommand{\sphinxmaketitle}{}
-            \renewcommand{\sphinxbackoftitlepage}{}
-            % Disable the table of contents
-            \renewcommand{\tableofcontents}{}
-            % Use the 'titlesec' package to remove chapter prefixes
-            \usepackage{titlesec}
-            \titleformat{\chapter}[display]
-                {\normalfont\huge\bfseries}{}{0pt}{\Huge}
-            \titleformat{name=\chapter,numberless}[display]
-                {\normalfont\huge\bfseries}{}{0pt}{\Huge}
-            % Customize the formatting for sections, if desired
-            \titleformat{\section}
-                {\normalfont\Large\bfseries}{\thesection}{1em}{}
-            % Customize the formatting for subsections, if desired
-            \titleformat{\subsection}
-                {\normalfont\large\bfseries}{\thesubsection}{1em}{}
-            % Customize the formatting for subsubsections, if desired
-            \titleformat{\subsubsection}
-                {\normalfont\normalsize\bfseries}{\thesubsubsection}{1em}{}
-            % Customize the formatting for paragraphs, if desired
-            \titleformat{\paragraph}[runin]
-                {\normalfont\normalsize\bfseries}{\theparagraph}{1em}{}
-            % Customize the formatting for subparagraphs, if desired
-            \titleformat{\subparagraph}[runin]
-                {\normalfont\normalsize\bfseries}{\thesubparagraph}{1em}{}
-        ''',
+            'preamble': TEXT_COMP,
 
     # Latex figure (float) alignment
     #'figure_align': 'htbp',
-        'babel': '% use given babel',
+        'babel': '',
+        'fontenc': '',
+        'inputenc': '',
+        'utf8extra': '',
+        'cmappkg': '',
+        'geometry': r'\usepackage{geometry}',
+        'hyperref': r'\usepackage{hyperref}',
     },
     latex_documents = [
-        ('index', 'qupulse.tex', 'qupulse Documentation', 'Simon Humpohl', 'manual'),
-    #    ('concepts/concepts', 'qupulse_concepts.tex', 'title_test', 'author_test', 'manual')
+    #    ('index', 'qupulse.tex', 'qupulse Documentation', 'Simon Humpohl', 'manual'),
+        ('concepts/concepts', 'qupulse_concepts.tex', 'title_test', 'author_test', 'manual'),
+    #    ('learners_guide', 'qupulse_learners_guide.tex', 'title_test', 'author_test', 'manual'),
+    #    ('_autosummary/qupulse', 'qupulse_reference.tex', 'title_test', 'author_test', 'manual')
     ],
     latex_domain_indices = False,
 )
 
+parser = argparse.ArgumentParser()
+parser.add_argument("targetdir", help="Directory to dump sphinx files into")
+parser.add_argument("--skip-build", default=False, action='store_true')
 
-if __name__ == '__main__':
+def extract_embeddable_documents(build_dir, target_dir):
+    target_dir = pathlib.Path(target_dir).absolute()
+    assert target_dir.exists()
+    assert (target_dir / 'main.tex').exists()
+    enc = 'utf-8'
+
+    for existing_sphinx_file in target_dir.glob("*sphinx*.*"):
+        existing_sphinx_file.unlink()
+
+    for sphinx_file in build_dir.glob('*sphinx*.*'):
+        if 'tableofcontents' in sphinx_file.read_text(enc):
+            print('tableofcontents', sphinx_file.name)
+
+        if 'sphinxlatexstyle' not in sphinx_file.stem:
+            shutil.copy(sphinx_file, target_dir)
+            txt = sphinx_file.read_text(enc)
+            if 'titlesec' in txt:
+                print(sphinx_file.name, 'contains titlesec')
+
+    sphinx_sty_path = target_dir / 'sphinx.sty'
+    sphinx_sty = sphinx_sty_path.read_text(enc)
+    sphinx_sty = '\n'.join(
+        line for line in sphinx_sty.splitlines()
+        if 'sphinxlatexstyle' not in line
+    )
+    sphinx_sty_path.write_text(sphinx_sty)
+
+    appendix_dir = target_dir / 'appendix' / 'qupulse_doc'
+
+    for png in build_dir.glob('*.png'):
+        shutil.copy(png, appendix_dir)
+
+    qupulse_source = (build_dir / 'qupulse.tex').read_text(encoding=enc)
+    header, rest = qupulse_source.split(r'\title', 1)
+    header = '\n'.join(line for line in header.splitlines()
+                       if not (line.startswith(r'\documentclass') or 'titlesec' in line or 'sphinxbackoftitlepage' in line))
+    _, body = rest.split(r'\begin{document}', 1)
+    body, _ = body.rsplit(r'\end{document}', 1)
+    body = body.replace(r'\sphinxAtStartPar', '')
+
+    (target_dir / 'qupulseappendix.sty').write_text(header, encoding=enc)
+    (appendix_dir / 'qupulseappendix.tex').write_text(body, encoding=enc)
+
+
+def build_latex():
     if thesis_source.exists():
         raise FileExistsError(f"Please delete {thesis_source}")
 
@@ -72,6 +133,8 @@ if __name__ == '__main__':
     shutil.copytree(original_source, thesis_source, ignore=shutil.ignore_patterns('*.pyc', '__pycache__', '_autosummary'))
     # ignore in git
     (thesis_source / '.gitignore').write_text("*")
+    thesis_build.mkdir(exist_ok=True)
+    (thesis_build / '.gitignore').write_text("*")
 
     with open(thesis_source / 'conf.py', 'a+') as fp:
         fp.write("\n\n# automatically appended")
@@ -79,10 +142,27 @@ if __name__ == '__main__':
             fp.write(f"{key} = {value!r}\n")
         fp.write("\n")
 
+    # remove index
+    index_file = thesis_source / 'index.rst'
+    index_contents = index_file.read_text()
+    index_contents, _ = index_contents.split("Indices and tables")
+    index_file.write_text(index_contents)
+
     try:
-        subprocess.check_call(["sphinx-build", "-blatex", "-j4", thesis_source.relative_to(doc_root), build.relative_to(doc_root)], cwd=doc_root)
+        subprocess.check_call(["sphinx-build", "-blatex", "-j4", thesis_source.relative_to(doc_root), thesis_build.relative_to(doc_root)], cwd=doc_root)
     finally:
         print("Deleting patched source directory")
         shutil.rmtree(thesis_source)
+
+
+if __name__ == '__main__':
+    parsed = parser.parse_args()
+
+    if not parsed.skip_build:
+        build_latex()
+
+    extract_embeddable_documents(thesis_build, parsed.targetdir)
+
+
 
 
